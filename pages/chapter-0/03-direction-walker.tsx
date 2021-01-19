7281f3eef5c87b899950587bf38e9d4993ddc094
gouflv <lv.gouf@gmail.com>
2021-01-19 16:37:13 +0800

    feature(chapter-0): init 03-direction-walker

---
+++ pages/chapter-0/03-direction-walker.tsx
@@ -0,0 +1,46 @@
+import { SketchProps } from 'react-p5'
+import { P5Render } from '../../components/P5Render'
+
+function createDrawer(): SketchProps {
+  let p: P5
+  let walker: Walker
+
+  class Walker {
+    constructor(private x: number, private y: number) {}
+    step() {
+      const dirX = p.mouseX > this.x ? 1 : -1
+      const dirY = p.mouseY > this.y ? 1 : -1
+      const r = p.random(1)
+      if (r > 0.8) {
+        this.x += dirX
+      } else {
+        this.x += Math.floor(p.random([-1, 0, 1]))
+      }
+      if (r > 0.8) {
+        this.y += dirY
+      } else {
+        this.y += Math.floor(p.random([-1, 0, 1]))
+      }
+    }
+    display() {
+      p.stroke(0)
+      p.point(this.x, this.y)
+    }
+  }
+
+  return {
+    setup: (p5, canvas) => {
+      p = p5
+      p.createCanvas(800, 500).parent(canvas)
+      walker = new Walker(p.width / 2, p.height / 2)
+    },
+    draw: () => {
+      if (walker) {
+        walker.step()
+        walker.display()
+      }
+    }
+  }
+}
+
+export default P5Render(createDrawer)
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,15 @@ export default function Home() {
       <h2>Chapter 0</h2>
       <ul>
         <li>
-          <Link href='/chapter-0/01-walker'>0-1 传统的随机游走</Link>
+          <Link href='/chapter-0/01-walker'>01 传统的随机游走</Link>
         </li>
         <li>
-          <Link href='/chapter-0/02-random'>0-2 随机数分布</Link>
+          <Link href='/chapter-0/02-random'>02 随机数分布</Link>
+        </li>
+        <li>
+          <Link href='/chapter-0/03-direction-walker'>
+            03 根据鼠标方向的趋势游走
+          </Link>
         </li>
       </ul>
     </div>
